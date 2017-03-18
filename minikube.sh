aba3b03c027b482a1954b184a7d9f06390f38ce1
Staffan Olsson <staffan@repos.se>
2017-03-18 14:50:40 +0100

    Tries to fix automatic setup by creating ingress after service and waiting for pods to go up

--- minikube.sh
+++ minikube.sh
@@ -2,7 +2,9 @@
 set -x
 set -e
 
-export KUBECONFIG=~/.kube/topics-test
+# Encourage the habit of environment specific kubeconfig, with no live cluster in default
+# export KUBECONFIG=~/.kube/topics-test
+[ -z "$KUBECONFIG" ] && echo "Bailing out because you're running a fallback kubeconfig" && exit 1
 
 minikube start
 
@@ -11,6 +13,10 @@ kubectl cluster-info
 minikube addons enable ingress
 
 kubectl create -f demo-k8s
+
+# curl will retry on 503, but not prior to that as the machine doesn't respond
+sleep 30
+curl -u reposetup: --retry 60 --retry-delay 5 http://192.168.99.101/svn/ -I 2>&1 | grep "HTTP"
 
 REPONAME=topics01
 kubectl exec -c svn svn-0 -- repocreate $REPONAME -o daemon
