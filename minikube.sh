8c157e30b3bcd64bc7e2b502e81079b1ff70755e
Staffan Olsson <staffan@repos.se>
2017-03-18 14:18:21 +0100

    Tweaks repository creation

--- minikube.sh
+++ minikube.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+set -e
 
 export KUBECONFIG=~/.kube/topics-test
 
@@ -10,6 +12,9 @@ minikube addons enable ingress
 
 kubectl create -f demo-k8s
 
-kubectl exec -c svn svn-0 -- repocreate topics01 -o daemon
+REPONAME=topics01
+kubectl exec -c svn svn-0 -- repocreate $REPONAME -o daemon
+# first repo access should be through apache to avoid rep-cache write issue at commit
+curl -u "reposetup:" http://$(minikube ip)/svn/$REPONAME/ | grep rev
 # indexing doesn't re-run repo discovery
 kubectl exec -c indexing svn-0 -- kill 1
