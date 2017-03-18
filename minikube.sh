2f59a8b432136b6d9d04ef73a23185203e8eae57
Staffan Olsson <staffan@repos.se>
2017-03-18 17:10:51 +0100

    Trying, unreliably, to verify that svn gets online through the ingress

--- minikube.sh
+++ minikube.sh
@@ -6,26 +6,32 @@ set -e
 # export KUBECONFIG=~/.kube/topics-test
 [ -z "$KUBECONFIG" ] && echo "Bailing out because you're running a fallback kubeconfig" && exit 1
 
+minikube start
+
 NAMESPACE="topics"
 CONTEXT=$(kubectl config view | grep current-context | awk '{print $2}')
 kubectl config set-context $CONTEXT --namespace=$NAMESPACE
 echo "namespace \"$NAMESPACE\" set."
 
-minikube start
-
+sleep 5
 kubectl cluster-info
 
 minikube addons enable ingress
 
 kubectl create -f demo-k8s/
 
-# curl will retry on 503, but not prior to that as the machine doesn't respond
+# Minikube will only manage to bind the volume if it is created after the StatefulSet
+kubectl delete -f demo-k8s/backend-rweb-pvc.yml
 sleep 30
+kubectl create -f demo-k8s/backend-rweb-pvc.yml
+
+# curl will retry on 503, but not prior to that as the machine doesn't respond
+sleep 60
 curl -u reposetup: --retry 60 --retry-delay 5 http://$(minikube ip)/svn/ -I 2>&1 | grep "HTTP"
 
 REPONAME=topics01
-kubectl exec -c svn svn-0 -- repocreate $REPONAME -o daemon
+kubectl -n $NAMESPACE exec -c svn svn-0 -- repocreate $REPONAME -o daemon
 # first repo access should be through apache to avoid rep-cache write issue at commit
 curl -u "reposetup:" http://$(minikube ip)/svn/$REPONAME/ -k | grep rev
 # indexing doesn't re-run repo discovery
-kubectl exec -c indexing svn-0 -- kill 1
+kubectl -n $NAMESPACE exec -c indexing svn-0 -- kill 1
