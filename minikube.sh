84be51abe4158929f2d050417a71118786de7569
Staffan Olsson <staffan@repos.se>
2017-03-18 14:15:17 +0100

    Restarts indexing after repository creation

--- minikube.sh
+++ minikube.sh
@@ -11,3 +11,5 @@ minikube addons enable ingress
 kubectl create -f demo-k8s
 
 kubectl exec -c svn svn-0 -- repocreate topics01 -o daemon
+# indexing doesn't re-run repo discovery
+kubectl exec -c indexing svn-0 -- kill 1
